8c087844f53e648b916f418a46b46d21060f9bd9
Giovanni Francesco Solone <dev@gfsolone.com>
2017-07-26 16:54:09 +0200

    Update 201707261654

--- ExportCompanyUser.ps1
+++ ExportCompanyUser.ps1
@@ -1,21 +1,23 @@
-############################################################################################################################
-# OFFICE 365: Export Company Users
-#----------------------------------------------------------------------------------------------------------------
-# Autore:				GSolone
-# Versione:				0.7
-# Utilizzo:				.\ExportCompanyUsers.ps1
-#						(opzionale, passaggio dati da prompt) .\ExportCompanyUsers.ps1 -RicercaDominio contoso.com
-#						(opzionale, passaggio dati da prompt) .\ExportCompanyUsers.ps1 -RicercaCompany "Contoso S.r.l."
-# Info:					http://gioxx.org/tag/o365-powershell
-# Ultima modifica:		11-10-2016
-# Modifiche:			
-#	0.7- ho modificato la modalità di esportazione dati, ora i risultati vanno in un file CSV. Ho commentato le righe relative all'aggiunta di data e ora dell'esportazione.
-#	0.6- prevedo utilizzo del parametro -RicercaCompany per filtrare un campo basato sulla Company e non sul Mail Domain.
-#	0.5- lo script accetta adesso i parametri passati da riga di comando (-RicercaDominio). Nuovo metodo di output dei dati trovati, ricerco prima le caselle, poi per ciascuna casella ricavo i dati che mi servono direttamente dallo User, permettendo così l'esportazione anche del campo Company. Chiedo se esportare i risultati in CSV (al posto di farlo per default).
-#	0.4- eliminato calcolo avanzamento download dati.
-#	0.3- correzione minore: la ricerca viene effettuata sullo specifico dominio in ingresso, un eventuale sottodominio deve essere dichiarato (esempio: contoso.com nella ricerca non mostrerà i risultati di dep1.contoso.com nell'eventualità dep1 fosse un suo sottodominio).
-#	0.2- stato di avanzamento in lettura / scrittura dati). Modificato il $_.EmailAddresses in $_.PrimarySmtpAddress per mettere la Company in base all'indirizzo di posta principale e non considerare eventuali alias
-############################################################################################################################
+<#
+	OFFICE 365: Export Company Users
+	----------------------------------------------------------------------------------------------------------------
+	Autore:				GSolone
+	Versione:			0.8
+	Utilizzo:			.\ExportCompanyUsers.ps1
+						(opzionale, passaggio dati da prompt) .\ExportCompanyUsers.ps1 -RicercaDominio contoso.com
+						(opzionale, passaggio dati da prompt) .\ExportCompanyUsers.ps1 -RicercaCompany "Contoso S.r.l." (comprensivo di virgolette)
+	Info:				http://gioxx.org/tag/o365-powershell
+	Ultima modifica:	21-07-2017
+	Modifiche:
+		0.8- riscritto, snellito e velocizzato.
+		0.7 rev1- ho solo corretto l'Out-String di uscita dei dati a video per RicercaDominio e RicercaCompany (ora -AutoSize)
+		0.7- ho modificato la modalità di esportazione dati, ora i risultati vanno in un file CSV. Ho commentato le righe relative all'aggiunta di data e ora dell'esportazione.
+		0.6- prevedo utilizzo del parametro -RicercaCompany per filtrare un campo basato sulla Company e non sul Mail Domain.
+		0.5- lo script accetta adesso i parametri passati da riga di comando (-RicercaDominio). Nuovo metodo di output dei dati trovati, ricerco prima le caselle, poi per ciascuna casella ricavo i dati che mi servono direttamente dallo User, permettendo così l'esportazione anche del campo Company. Chiedo se esportare i risultati in CSV (al posto di farlo per default).
+		0.4- eliminato calcolo avanzamento download dati.
+		0.3- correzione minore: la ricerca viene effettuata sullo specifico dominio in ingresso, un eventuale sottodominio deve essere dichiarato (esempio: contoso.com nella ricerca non mostrerà i risultati di dep1.contoso.com nell'eventualità dep1 fosse un suo sottodominio).
+		0.2- stato di avanzamento in lettura / scrittura dati). Modificato il $_.EmailAddresses in $_.PrimarySmtpAddress per mettere la Company in base all'indirizzo di posta principale e non considerare eventuali alias
+#>
 
 #Verifica parametri da prompt
 Param( 
@@ -44,8 +46,8 @@ Function Main {
 			Write-Host "        Azienda di ricerca: " -f "White" -nonewline; Write-Host "$RicercaCompany" -f "Green"
 			Write-Progress -Activity "Download dati da Exchange" -Status "Ricerco le caselle con appartenenti al gruppo che mi hai richiesto, attendi..."
 			
-			$RicercaMailbox = Get-User -ResultSize Unlimited | where {$_.Company -eq "$RicercaCompany"}
-			$RicercaMailbox | foreach {Get-User $_.Name} | ft DisplayName, WindowsEmailAddress, Company, City | Out-String
+			""; Get-Recipient -ResultSize Unlimited | where {$_.Company -eq "$RicercaCompany"} | Select-Object DisplayName, PrimarySmtpAddress, Company, City | Sort-Object PrimarySmtpAddress
+			""
 			
 			# Chiedo se esportare i risultati in un file CSV
 			$ExportList = "C:\temp\$RicercaCompany.csv"
@@ -58,11 +60,9 @@ Function Main {
 				Write-Host "Esporto l'elenco in C:\temp\$RicercaCompany.csv e apro il file (salvo errori)." -f "Green"
 				$Today = [string]::Format( "{0:dd/MM/yyyy}", [datetime]::Now.Date )
 				$TimeIs = (get-date).tostring('HH:mm:ss')
-				$RicercaMailbox | foreach {Get-User $_.Name} | Select-Object -Property @{label="DisplayName";expression={$_.DisplayName}}, @{label="WindowsEmailAddress";expression={$_.WindowsEmailAddress}}, @{label="Company";expression={$_.Company}}, @{label="City";expression={$_.City}} | Export-CSV $ExportList -force -notypeinformation
+				Get-Recipient -ResultSize Unlimited | where {$_.Company -eq "$RicercaCompany"} | Select-Object DisplayName, PrimarySmtpAddress, Company, City | Export-CSV $ExportList -force -notypeinformation
 				$a = Get-Content $ExportList
 				$b = "Esportazione utenti $RicercaCompany - $Today alle ore $TimeIs"
-				#Set-Content $ExportList –value $b, $a[0..18]
-				#Set-Content $ExportList –value $a, $b
 				Invoke-Item $ExportList
 			}
 			""		
@@ -76,8 +76,8 @@ Function Main {
 			Write-Host "        Dominio di ricerca: " -f "White" -nonewline; Write-Host "$RicercaDominio" -f "Green"
 			Write-Progress -Activity "Download dati da Exchange" -Status "Ricerco le caselle con il dominio che mi hai richiesto, attendi..."
 			
-			$RicercaMailbox = Get-Mailbox -ResultSize Unlimited | where {$_.PrimarySmtpAddress -like "*@" + $RicercaDominio}
-			$RicercaMailbox | foreach {Get-User $_.Name} | ft DisplayName, WindowsEmailAddress, Company, City | Out-String
+			""; Get-Recipient -ResultSize Unlimited | where {$_.Company -eq "$RicercaCompany"} | Select-Object DisplayName, PrimarySmtpAddress, Company, City | Sort-Object PrimarySmtpAddress
+			""
 			
 			# Chiedo se esportare i risultati in un file CSV
 			$ExportList = "C:\temp\$RicercaDominio.csv"
@@ -90,11 +90,9 @@ Function Main {
 				Write-Host "Esporto l'elenco in C:\temp\$RicercaDominio.csv e apro il file (salvo errori)." -f "Green"
 				$Today = [string]::Format( "{0:dd/MM/yyyy}", [datetime]::Now.Date )
 				$TimeIs = (get-date).tostring('HH:mm:ss')
-				$RicercaMailbox | foreach {Get-User $_.Name} | Select-Object -Property @{label="DisplayName";expression={$_.DisplayName}}, @{label="WindowsEmailAddress";expression={$_.WindowsEmailAddress}}, @{label="Company";expression={$_.Company}}, @{label="City";expression={$_.City}} | Export-CSV $ExportList -force -notypeinformation
+				Get-Recipient -ResultSize Unlimited | where {$_.Company -eq "$RicercaCompany"} | Select-Object DisplayName, PrimarySmtpAddress, Company, City | Export-CSV $ExportList -force -notypeinformation
 				$a = Get-Content $ExportList
 				$b = "Esportazione utenti $RicercaDominio - $Today alle ore $TimeIs"
-				#Set-Content $ExportList –value $b, $a[0..18]
-				#Set-Content $ExportList –value $a, $b
 				Invoke-Item $ExportList
 			}
 			""		
--- ListDynamicDistrGroup.ps1
+++ ListDynamicDistrGroup.ps1
@@ -2,16 +2,17 @@
 # OFFICE 365: Show Dynamic Distribution Group Users
 #----------------------------------------------------------------------------------------------------------------
 # Autore:				GSolone
-# Versione:				0.3 rev1
+# Versione:				0.3 rev2
 # Utilizzo:				.\ListDynamicDistrGroup.ps1
 #						(opzionale, passaggio dati da prompt) .\ListDynamicDistrGroup.ps1 group@contoso.com
-#						(opzionale, passaggio dati da prompt) .\ListDynamicDistrGroup.ps1 group@contoso.com -TXT C:\temp\group.txt
+#						(opzionale, passaggio dati da prompt) .\ListDynamicDistrGroup.ps1 group@contoso.com -CSV C:\temp\group.CSV
 # Info:					http://gioxx.org/tag/o365-powershell
-# Ultima modifica:		16-09-2016
+# Ultima modifica:		26-07-2017
 # Modifiche:			
-#	0.3 rev1- mostro a video le informazioni che prima esportavo esclusivamente nel TXT finale.
-#	0.3- l'esportazione su TXT include anche l'indirizzo SMTP primario.
-#	0.2- lo script accetta adesso i parametri passati da riga di comando (-RicercaGruppo e -TXT), e permette di esportare il risultato della query su file TXT.
+#	0.3 rev2- Sort-Object sull'output. Esportazione in CSV e non più su file di testo.
+#	0.3 rev1- mostro a video le informazioni che prima esportavo esclusivamente nel CSV finale.
+#	0.3- l'esportazione su CSV include anche l'indirizzo SMTP primario.
+#	0.2- lo script accetta adesso i parametri passati da riga di comando (-RicercaGruppo e -CSV), e permette di esportare il risultato della query su file CSV.
 ############################################################################################################################
 
 #Verifica parametri da prompt
@@ -19,7 +20,7 @@ Param(
     [Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true)] 
     [string] $RicercaGruppo, 
     [Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)] 
-    [string] $TXT
+    [string] $CSV
 )
 
 #Main
@@ -43,33 +44,33 @@ Function Main {
 	{
 		Write-Progress -Activity "Download dati da Exchange" -Status "Esporto utenti in $RicercaGruppo, attendi..."
 		$members = Get-DynamicDistributionGroup -Identity $RicercaGruppo
-		Get-Recipient -RecipientPreviewFilter $members.RecipientFilter | ft Name, PrimarySmtpAddress, Company, City
+		""; Get-Recipient -RecipientPreviewFilter $members.RecipientFilter | Select Name, PrimarySmtpAddress, Company, City | Sort-Object PrimarySmtpAddress
 		""
 		
-		# Esportazione risultati in TXT
-		$message = "Vuoi esportare il risultato in un file TXT? (default: NO)"
+		# Esportazione risultati in CSV
+		$message = "Vuoi esportare il risultato in un file CSV? (default: NO)"
 		$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Si."
 		$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Non ora."
 		$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 		$result = $host.ui.PromptForChoice("", $message, $options, 1)
 		if ($result -eq 0) { 
 			""
-			if ( [string]::IsNullOrEmpty($TXT) )
+			if ( [string]::IsNullOrEmpty($CSV) )
 				{
-					# Directory esportazione TXT non specificata, utilizzo default
-					$TXT = "C:\temp\$RicercaGruppo.txt"
+					# Directory esportazione CSV non specificata, utilizzo default
+					$CSV = "C:\temp\$RicercaGruppo.CSV"
 				}
-			Write-Host "Esporto i risultati in $TXT :" -nonewline -f "yellow"
-			Get-Recipient -RecipientPreviewFilter $members.RecipientFilter | ft Name, PrimarySmtpAddress, Company, City > $TXT
+			Write-Host "Esporto i risultati in $CSV :" -nonewline -f "yellow"
+			Get-Recipient -RecipientPreviewFilter $members.RecipientFilter | Select Name, PrimarySmtpAddress, Company, City | Export-CSV $CSV -notypeinformation -force
 			Write-Host " fatto" -f "green"
 			""
-			# Richiedo apertura file TXT
-			$message = "Devo aprire il file TXT generato? (default: NO)"
+			# Richiedo apertura file CSV
+			$message = "Devo aprire il file CSV generato? (default: NO)"
 			$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Si."
 			$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Non ora."
 			$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 			$result = $host.ui.PromptForChoice("", $message, $options, 1)
-			if ($result -eq 0) { Invoke-Item $TXT }
+			if ($result -eq 0) { Invoke-Item $CSV }
 		}
 		""
 	}
