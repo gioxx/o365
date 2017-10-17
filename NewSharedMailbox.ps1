1d99466ad0cb0f61c6dff16daae31628d6c956c4
Giovanni Francesco Solone <dev@gfsolone.com>
2017-10-17 14:49:56 +0200

    Update 201710171449

--- NewSharedMailbox.ps1
+++ NewSharedMailbox.ps1
@@ -1,87 +1,74 @@
-############################################################################################################################
-# OFFICE 365: Create New Shared Mailbox
-#----------------------------------------------------------------------------------------------------------------
-# Original Author: 		Alan Byrne
-# Version: 				1.0
-# Last Modified Date: 	18/09/2012
-# Last Modified By: 	Alan Byrne
-#----------------------------------------------------------------------------------------------------------------
-# Adattata da:			GSolone
-# Versione:				0.7
-# Utilizzo:				.\New-SharedMailbox.ps1
-# Info:					http://gioxx.org/tag/o365-powershell
-# Ultima modifica:		13-10-2015
-# Modifiche:
-#	0.7-ho commentato la forzatura del MicrosoftOnlineServicesID, non più necessaria
-#	0.6-ho dovuto inserire un nuovo "sleep" prima di settare il MicrosoftOnlineServicesID perché in alcuni casi il server Exchange non trova immediatamente l'utente, cosa che invece succede già a 10 secondi di distanza dal comando di creazione casella.
-#	0.5-correzioni minori
-#	0.1/0-4-rimosso limite Shared a 5GB, forzata la richiesta dell'alias da utilizzare, rimossa richiesta permission Send As e Automap (entrambe dati per scontati e impostati di default), forzato il MicrosoftOnlineServicesID sull'indirizzo completo di posta elettronica
-############################################################################################################################
+<#
+	OFFICE 365: Create New Shared Mailbox
+	----------------------------------------------------------------------------------------------------------------
+	Original Author: 		Alan Byrne
+	Version: 				1.0
+	Last Modified Date: 	18/09/2012
+	Last Modified By: 		Alan Byrne
+	----------------------------------------------------------------------------------------------------------------
+	Autore:					GSolone
+	Versione:				0.8
+	Utilizzo:				.\New-SharedMailbox.ps1
+	Info:					http://gioxx.org/tag/o365-powershell
+	Ultima modifica:		17-10-2017
+	Modifiche:
+		0.8- ho modificato l'esempio relativo all'indirizzo di posta da creare (ora info@contoso.com). Ho messo a posto parte dell'indentazione, e ho aggiunto un blocco che mostra i permessi applicati alla casella di posta quando si termina di aggiungerne.
+		0.7- ho commentato la forzatura del MicrosoftOnlineServicesID, non più necessaria
+		0.6- ho dovuto inserire un nuovo "sleep" prima di settare il MicrosoftOnlineServicesID perché in alcuni casi il server Exchange non trova immediatamente l'utente, cosa che invece succede già a 10 secondi di distanza dal comando di creazione casella.
+		0.5- correzioni minori
+		0.1/0-4- rimosso limite Shared a 5GB, forzata la richiesta dell'alias da utilizzare, rimossa richiesta permission Send As e Automap (entrambe dati per scontati e impostati di default), forzato il MicrosoftOnlineServicesID sull'indirizzo completo di posta elettronica
+#>
 
 #Main
 Function Main {
 
 	""
-	Write-Host "        Office 365: New Shared Mailbox" -foregroundcolor "green"
+	Write-Host "        Office 365: New Shared Mailbox" -f "Green"
 	Write-Host "        ------------------------------------------"
-	Write-Host "          ATTENZIONE:" -foregroundcolor "red"
-	Write-Host "          Fare molta attenzione ai possibili errori di digitazione" -foregroundcolor "red"
-	Write-Host "          nei dati richiesti qui di seguito" -foregroundcolor "red"
+	Write-Host "          ATTENZIONE:" -f "Red"
+	Write-Host "          Fare molta attenzione ai possibili errori di digitazione" -f "Red"
+	Write-Host "          nei dati richiesti qui di seguito" -f "Red"
 	""
 	Write-Host "-------------------------------------------------------------------------------------------------"
-	$SharedMailboxUserName = Read-Host "Indirizzo (esempio: info@domain.tld)        "
+	$SharedMailboxUserName = Read-Host "Indirizzo (esempio: info@contoso.com)       "
 	$SharedMailboxDisplayName = Read-Host "Nome casella (esempio: Contoso srl -Info)   "
-	#Forzo richiesta alias
 	$SharedMailboxAlias = Read-Host "Alias (esempio: Contososrl_info)            "
 	
-	#Create the new Shared mailbox
 	try
 	{
-		""
-		Write-Host "Creo la Shared Mailbox, attendi." -foregroundcolor "yellow"
-		""
+		""; Write-Host "Creo la Shared Mailbox, attendi." -f "Yellow"; ""
 		New-Mailbox -Name $SharedMailboxDisplayName -Alias $SharedMailboxAlias -Shared -PrimarySMTPAddress $SharedMailboxUserName
 		Start-Sleep -s 10
-		#Forzo il MicrosoftOnlineServicesID (per evitare il default NOME@DOMINIO.onmicrosoft.com)
-		#Set-Mailbox $SharedMailboxDisplayName -MicrosoftOnlineServicesID $SharedMailboxUserName
 	}
 	catch
 	{
-		Write-Host "Non riesco a creare la Shared Mailbox, riprovare." -foregroundcolor "red"
+		Write-Host "Non riesco a creare la Shared Mailbox, riprovare." -f "Red"
 		write-host $error[0]
 		return ""
 	}
 	
-	Start-Sleep -s 10
-	""
-	Write-Host "Shared Mailbox creata correttamente: $SharedMailboxUserName" -foregroundcolor "green"
-	""
+	""; Write-Host "Shared Mailbox creata correttamente: $SharedMailboxUserName" -f "Green"; ""
 	
 	#Prompt user to add permissions to mailbox
 	$title = "PERMESSI"
 	$message = "Vuoi aggiungere adesso i permessi di accesso alla casella?"
-
 	$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Aggiungi permessi."
 	$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Non ora."
 	$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-
 	$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
 	
 	while ($result -eq 0) 
 	{
-		""
-		Write-Host ""
+		""; "";
 		#User to add to mailbox
 		$UserToPermission = Read-Host "Inserisci il nome utente della persona o del gruppo al quale intendi dare accesso alla casella (esempio: mario.rossi@domain.tld o Mario Rossi)"
 		
 		#Get Permission Type
 		$title = "TIPO PERMESSI"
 		$message = "Che tipo di permesso vuoi applicare all'utente appena dichiarato?"
-
 		$FullAccess = New-Object System.Management.Automation.Host.ChoiceDescription "&Full Access", ""
 		$ReadOnly = New-Object System.Management.Automation.Host.ChoiceDescription "&Read Only", ""
 		$options = [System.Management.Automation.Host.ChoiceDescription[]]($FullAccess, $ReadOnly)
-
 		$PermissionType = $host.ui.PromptForChoice($title, $message, $options, 0)
 		
 		#Set the Permissions
@@ -89,23 +76,13 @@ Function Main {
 		{
 			try
 			{
-				""
-				Write-Host ""
-				Write-Host "Inserisco i permessi Full Access sulla shared mailbox"
-				""
-#		Blocco il controllo variabile Automap.
-#				if ($AutoMap -eq 0)
-#				{
-					Add-MailboxPermission $SharedMailboxDisplayName -User $UserToPermission -AccessRights FullAccess -AutoMapping $true
-#				}
-#				else
-#				{	
-#					Add-MailboxPermission $SharedMailboxDisplayName -User $UserToPermission -AccessRights FullAccess -AutoMapping $false
-#				}
+				""; "";
+				Write-Host "Inserisco i permessi Full Access sulla shared mailbox"; "";
+				Add-MailboxPermission $SharedMailboxDisplayName -User $UserToPermission -AccessRights FullAccess -AutoMapping $true
 			}
 			catch
 			{
-				Write-Host "Non riesco ad applicare i permessi Full Access, riprovare." -foregroundcolor "red"
+				Write-Host "Non riesco ad applicare i permessi Full Access, riprovare." -f "Red"
 				write-host $error[0]
 				return ""
 			}
@@ -114,54 +91,42 @@ Function Main {
 		{
 			try
 			{
-				""
-				Write-Host ""
-				Write-Host "Inserisco i permessi Read Only sulla shared mailbox"
-				""
-#		Blocco il controllo variabile Automap.
-#				if ($AutoMap -eq 0)
-#				{
-					Add-MailboxPermission $SharedMailboxDisplayName -User $UserToPermission -AccessRights ReadPermission -AutoMapping $true
-#				}
-#				else
-#				{	
-#					Add-MailboxPermission $SharedMailboxDisplayName -User $UserToPermission -AccessRights ReadPermission -AutoMapping $false
-#				}
+				""; ""
+				Write-Host "Inserisco i permessi Read Only sulla shared mailbox"; ""
+				Add-MailboxPermission $SharedMailboxDisplayName -User $UserToPermission -AccessRights ReadPermission -AutoMapping $true
 			}
 			catch
 			{
-				Write-Host "Non riesco ad applicare i permessi Read Only, riprovare." -foregroundcolor "red"
+				Write-Host "Non riesco ad applicare i permessi Read Only, riprovare." -f "Red"
 				write-host $error[0]
 				return ""
 			}
 		}
 		
-#		Blocco il controllo variabile Send As.
-#		#Set sendas permissions if required
-#		if ($SendAs -eq 0)
-#		{
-			try
-			{
-				Add-RecipientPermission $SharedMailboxDisplayName -Trustee $UserToPermission -AccessRights SendAs
-			}
-			catch
-			{
-				Write-Host "Non riesco ad applicare i permessi 'invia come', riprovare." -foregroundcolor "red"
-				write-host $error[0]
-				return ""
-			}
-#		}
+		try
+		{
+			Add-RecipientPermission $SharedMailboxDisplayName -Trustee $UserToPermission -AccessRights SendAs
+		}
+		catch {
+			Write-Host "Non riesco ad applicare i permessi 'invia come', riprovare." -f "Red"
+			write-host $error[0]
+			return ""
+		}
 
 		#See if user wants to add additional permissions to mailbox
 		$title = "ALTRI PERMESSI"
 		$message = "Vuoi aggiungere ulteriori permessi alla casella creata?"
-
 		$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Aggiungi permessi."
 		$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Non ora."
 		$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-
 		$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
 	}
+	
+	""; Write-Host "All Done!" -f "Green"
+	Write-Host "Riepilogo accessi alla casella di $SharedMailboxUserName " -f "Yellow"
+	Get-MailboxPermission -Identity $SharedMailboxUserName | where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.user.tostring() -NotLike "S-1-5*" -and $_.IsInherited -eq $false} | Select Identity,User,AccessRights | ft User,AccessRights | out-string
+	Get-RecipientPermission $SharedMailboxUserName -AccessRights SendAs | where {$_.Trustee.tostring() -ne "NT AUTHORITY\SELF" -and $_.Trustee.tostring() -NotLike "S-1-5*"} | ft Trustee, AccessRights | out-string
+	
 }
 
 # Start script
--- RemoveOrphanedSID-CSV.ps1
+++ RemoveOrphanedSID-CSV.ps1
@@ -1,33 +1,35 @@
-############################################################################################################################
-# OFFICE 365: Remove Orphaned SID (Bulk, CSV)
-#----------------------------------------------------------------------------------------------------------------
-# Autore:				GSolone
-# Versione:				0.4 rev2
-# Utilizzo:				.\RemoveOrphanedSID-CSV.ps1
-#						(opzionale, modifica posizione file CSV) .\RemoveOrphanedSID-CSV.ps1 -csv C:\Export.CSV
-#						(opzionale, analisi singola casella) .\RemoveOrphanedSID-CSV.ps1 -mbox shared@contoso.com
-#						(opzionale, avvio rimozione) .\RemoveOrphanedSID-CSV.ps1 -action remove
-#						I parametri da prompt possono essere concatenati.
-# Info:					http://gioxx.org/tag/o365-powershell
-# Ultima modifica:		25-08-2016
-# Modifiche:
-#	0.4 rev2- banale modifica per correzione estetica al "Premi un tasto per continuare" (mancavano le righe vuote alla conferma).
-#	0.4 rev1- aggiunta funzione di Pausa per evitare di intercettare il tasto CTRL.
-#	0.4- corretto errore che non esportava in CSV temporaneo i SID orfani di una singola mailbox (.\RemoveOrphanedSID-CSV.ps1 -mbox shared@contoso.com)
-#	0.3- prevedo concatenamento del file CSV da utilizzare con Action di Remove. Così facendo salto l'esportazione. Esempio di utilizzo: .\RemoveOrphanedSID-CSV.ps1 -csv C:\temp\OrphanedSID.csv -action remove. Corretti errori minori, modificata indentazione dello script. Richiedo ora cancellazione della lista CSV al termine di una Action di Remove.
-#	0.2 rev1- correzioni minori (maggiori informazioni di utilizzo nel messaggio a video quando si lancia lo script).		
-#	0.2- le condizioni di verifica fanno ora scomparire / comparire istruzioni a video. Prevedo l'utilizzo di un file CSV temporaneo nel caso in cui si vada a filtrare una sola mailbox con action remove. In caso contrario, in base a quanti e quali parametri utilizzo, mi comporto diversamente. Il file temporaneo (CSV) di esportazione SID orfani di singola casella, viene poi eliminato al termine dell'operazione.
-#	0.1 rev1- ho solo corretto un errore in un messaggio di informazione.
-############################################################################################################################
+<#
+	OFFICE 365: Remove Orphaned SID (Bulk, CSV)
+	------------------------------------------------------------------------------------------------------------
+	Autore:				GSolone
+	Versione:			0.5
+	Utilizzo:			.\RemoveOrphanedSID-CSV.ps1
+						(opzionale, modifica posizione file CSV) .\RemoveOrphanedSID-CSV.ps1 -csv C:\Export.CSV
+						(opzionale, analisi singola casella) .\RemoveOrphanedSID-CSV.ps1 -mbox shared@contoso.com
+						(opzionale, avvio rimozione) .\RemoveOrphanedSID-CSV.ps1 -Remove
+						I parametri da prompt possono essere concatenati.
+	Info:				https://gioxx.org/tag/o365-powershell
+	Ultima modifica:	26-09-2017
+	------------------------------------------------------------------------------------------------------------
+	== Modifiche ==
+		0.5- sostituito il parametro Action con uno switch diretto "Remove". Messo a posto formattazione script in più punti. Prevista la creazione della lista SID orfani in CSV nel caso in cui non sia presente (perché precedentemente creata) nella cartella di appoggio ($ExportList).
+		0.4 rev2- banale modifica per correzione estetica al "Premi un tasto per continuare" (mancavano le righe vuote alla conferma).
+		0.4 rev1- aggiunta funzione di Pausa per evitare di intercettare il tasto CTRL.
+		0.4- corretto errore che non esportava in CSV temporaneo i SID orfani di una singola mailbox (.\RemoveOrphanedSID-CSV.ps1 -mbox shared@contoso.com)
+		0.3- prevedo concatenamento del file CSV da utilizzare con Action di Remove. Così facendo salto l'esportazione. Esempio di utilizzo: .\RemoveOrphanedSID-CSV.ps1 -csv C:\temp\OrphanedSID.csv -action remove. Corretti errori minori, modificata indentazione dello script. Richiedo ora cancellazione della lista CSV al termine di una Action di Remove.
+		0.2 rev1- correzioni minori (maggiori informazioni di utilizzo nel messaggio a video quando si lancia lo script).		
+		0.2- le condizioni di verifica fanno ora scomparire / comparire istruzioni a video. Prevedo l'utilizzo di un file CSV temporaneo nel caso in cui si vada a filtrare una sola mailbox con action remove. In caso contrario, in base a quanti e quali parametri utilizzo, mi comporto diversamente. Il file temporaneo (CSV) di esportazione SID orfani di singola casella, viene poi eliminato al termine dell'operazione.
+		0.1 rev1- ho solo corretto un errore in un messaggio di informazione.
+#>
 
 #Verifica parametri da prompt
 Param( 
-    [Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true)] 
+    [Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true)]
     [string] $CSV,
-    [Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)] 
-    [string] $Action,
-	[Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$true)] 
-    [string] $Mbox
+	[Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)]
+    [string] $Mbox,
+	[Parameter(Position=2, Mandatory=$false)]
+    [switch] $Remove
 )
 
 #Main
@@ -45,16 +47,20 @@ Function Main {
 	################################################################################################
 
 	""
-	Write-Host "        Office 365: Remove Orphaned SID (CSV)" -foregroundcolor "green"
+	Write-Host "        Office 365: Remove Orphaned SID (CSV)" -f "green"
 	Write-Host "        ------------------------------------------"
 	Write-Host "         Lo script si occupa di analizzare, esportare e/o rimuovere i SID orfani"
-	Write-Host "         rimasti memorizzati nelle Mailbox Permission sulle caselle presenti in Exchange"
-	Write-Host "         (vedi commenti nello script per conoscere i parametri disponibili da prompt)"
-	Write-Host "         Utilizzo: .\RemoveOrphanedSID-CSV.ps1 -csv C:\Export.CSV"
-	Write-Host "                   .\RemoveOrphanedSID-CSV.ps1 -mbox shared@contoso.com"
-	Write-Host "                   .\RemoveOrphanedSID-CSV.ps1 -action remove"
-	Write-Host "         Vale concatenare i possibili parametri (es. -mbox shared@contoso.com -action remove)"
-	Write-Host "                                                (es. -csv C:\temp\OrphanedSID.csv -action remove)"
+	Write-Host "         rimasti memorizzati nelle Mailbox Permission delle caselle presenti in Exchange"
+	Write-Host "         Utilizzo:  .\RemoveOrphanedSID-CSV.ps1" -f "Yellow"
+	Write-Host "                    (esporta tutti i SID orfani in un file CSV di default)"	
+	Write-Host "         Opzionali: .\RemoveOrphanedSID-CSV.ps1 -CSV C:\Export.CSV" -f "Yellow"
+	Write-Host "                    (esporta tutti i SID orfani in un file CSV nella directory da te specificata)"
+	Write-Host "                    .\RemoveOrphanedSID-CSV.ps1 -Mbox shared@contoso.com" -f "Yellow"
+	Write-Host "                    (esporta i SID orfani della singola mailbox specificata)"
+	Write-Host "                    .\RemoveOrphanedSID-CSV.ps1 -Remove" -f "Yellow"
+	Write-Host "                    (esporta e rimuove i SID orfani da ogni casella presente in Exchange)"	
+	Write-Host "         Vale concatenare i possibili parametri (es. -mbox shared@contoso.com -Remove)"
+	Write-Host "                                                (es. -csv C:\temp\OrphanedSID.csv -Remove)"
 	""
 	
 try
@@ -67,7 +73,7 @@ try
 	# Notifico posizione file CSV
 	Write-Host "         File CSV in uso: $ExportList " -f "Green"; "";
 	
-	if ([string]::IsNullOrEmpty($Action)) {
+	if (-not($Remove)) {
 		# Procedo con l'esportazione dei SID orfani di tutte le caselle di posta elettronica
 		# (Nessuna Action specificata, quindi è certamente un'azione di esportazione).
 		Write-Host "         L'esportazione potrebbe richiedere molti minuti"
@@ -91,9 +97,9 @@ try
 	
 	"";"";
 	
-	if ([string]::IsNullOrEmpty($Action)) {
+	if (-not($Remove)) {
 	# Esporto i SID orfani SOLO se non è prevista una Action di Remove
-		Write-Progress -Activity "Download dati da Exchange" -Status "Ricerco ed esporto i SID in $ExportList , attendi..."
+		Write-Progress -Activity "Download dati da Exchange" -Status "Ricerco ed esporto i SID in $($ExportList), attendi..."
 		
 		if ([string]::IsNullOrEmpty($Mbox)) {
 		# Nessuna mailbox specificata, estraggo i SID di tutte le caselle in Exchange
@@ -104,19 +110,19 @@ try
 			Write-Host "         Analizzo la casella di posta $Mbox ..." -f "Yellow"
 			Get-Mailbox $Mbox | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user
 			""
-			if ([string]::IsNullOrEmpty($Action)) {
-				# Uso il file CSV di default o dichiarato nel caso in cui non ci sia un'action dichiarata
-				Write-Host "         Esporto i risultati nel file CSV $ExportList ..." -f "Yellow"
-				Get-Mailbox $Mbox | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user | Export-CSV -NoTypeInformation $ExportList
-			} else {
+			if ($Remove) {
 				Write-Host "         Esporto i risultati nel file CSV temporaneo ..." -f "Yellow"
 				# Altrimenti mi appoggio al file temporaneo che servirà a lanciare l'action in seguito
 				Get-Mailbox $Mbox | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user | Export-CSV -NoTypeInformation C:\temp\OrphSidTemp.csv
+			} else {
+				# Uso il file CSV di default o dichiarato nel caso in cui non ci sia un'action dichiarata
+				Write-Host "         Esporto i risultati nel file CSV $ExportList ..." -f "Yellow"
+				Get-Mailbox $Mbox | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user | Export-CSV -NoTypeInformation $ExportList
 			}
 		}
 		""; Write-Host "         Done." -f "Green"; "";
 		# Chiedo se visualizzare i risultati esportati nel file CSV solo se NON specifico un'Action da prompt
-		$message = "Devo aprire il file CSV $ExportList ?"
+		$message = "Devo aprire il file CSV $($ExportList)?"
 		$Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", ""
 		$No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", ""
 		$options = [System.Management.Automation.Host.ChoiceDescription[]]($Yes, $No)
@@ -130,20 +136,24 @@ try
 #######################################################################################################################		
 
 	# Remove dei SID (NON SPECIFICO PER SINGOLA MAILBOX), chiedo autorizzazione a procedere prima di continuare
-	if ($Action -eq "remove" -and [string]::IsNullOrEmpty($Mbox)) {
+	if ($Remove -and [string]::IsNullOrEmpty($Mbox)) {
 		if ([string]::IsNullOrEmpty($CSV)) {
 			# NESSUN CSV SPECIFICATO COME SORGENTE DATI
 			# (prendo per buona l'ultimo file CSV specificato, ammesso che sia stato fatto, altrimenti il default)
-			Write-Host "Prima di procedere con la rimozione ricordarsi di effettuare l'estrazione in CSV" -f "Red"; "";
-			$message = "Posso procedere con la rimozione dei SID orfani esportati in $ExportList ?"
+			if((Test-Path $ExportList) -eq 0) {
+				Write-Host "File $ExportList non trovato, procedo con l'esportazione dei SID orfani ..." -f "Red"
+				Write-Progress -Activity "Download dati da Exchange" -Status "Ricerco ed esporto i SID in $($ExportList), attendi..."
+				Get-Mailbox -ResultSize Unlimited | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user | Export-CSV -NoTypeInformation $ExportList
+			}
+			$message = "Posso procedere con la rimozione dei SID orfani esportati in $($ExportList)?"
 		} else {
 			# CSV SPECIFICATO COME SORGENTE DATI
 			# (quindi rimuovo i SID orfani delle caselle salvate nella lista specificata)
 			Write-Host "File CSV da riga di comando: $ExportList" -f "Green"; "";
 			$message = "Posso procedere con la rimozione dei SID orfani?"
 		}
-		$Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", ""
-		$No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", ""
+		$Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Procedi"
+		$No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Ferma il processo"
 		$options = [System.Management.Automation.Host.ChoiceDescription[]]($Yes, $No)
 		$StartEngine = $host.ui.PromptForChoice("", $message, $options, 0)
 		if ($StartEngine -eq 0) {
@@ -155,16 +165,16 @@ try
 		
 		# Posso cancellare il file CSV utilizzato per rimuovere i SID orfani?
 		""; ""; Write-Host "Il lavoro dovrebbe essere terminato." -f "Green"
-		$message = "Posso procedere con la cancellazione del file $ExportList ?"
-		$Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", ""
-		$No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", ""
+		$message = "Posso procedere con la cancellazione del file temporaneo $($ExportList)?"
+		$Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Elimina il file"
+		$No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Non eliminare il file"
 		$options = [System.Management.Automation.Host.ChoiceDescription[]]($Yes, $No)
 		$CancellazioneCSV = $host.ui.PromptForChoice("", $message, $options, 0)
 		if ($CancellazioneCSV -eq 0) { Remove-Item $ExportList }
 	}
 	
 	# Remove dei SID per singola Mailbox specificata a riga di comando
-	if ($Action -eq "remove" -and [string]::IsNullOrEmpty($Mbox) -eq $false) {
+	if ($Remove -and [string]::IsNullOrEmpty($Mbox) -eq $false) {
 		Write-Host "Analizzo la casella di posta $Mbox ..." -f "Yellow"
 		Get-Mailbox $Mbox | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user | Export-CSV -NoTypeInformation C:\temp\OrphSidTemp.csv
 		Get-Mailbox $Mbox | Get-MailboxPermission | where {$_.accessrights -eq "FullAccess" -and $_.user -like "S-1-5-21*"} | Select-object identity,user
@@ -181,7 +191,7 @@ try
 }
 catch
 {
-	Write-Host "Errore nell'operazione, riprovare." -foregroundcolor "red"
+	Write-Host "Errore nell'operazione, riprovare." -f "red"
 	write-host $error[0]
 	return ""
 }
