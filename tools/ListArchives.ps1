ef4c7501213e7f78053f5cc2f8448b475f53c3ec
Gioxx <giovanni@solone.it>
2022-02-17 09:39:34 +0100

    Update ListArchives.ps1

--- tools/ListArchives.ps1
+++ tools/ListArchives.ps1
@@ -1,21 +1,13 @@
 <#
 	O365 PShell Snippet: Get Online Archive List
 	----------------------------------------------------------------------------------------------------------------
-	Autore:		GSolone (0.2 ult.mod. 5/2/20)
+	Autore:		GSolone (0.3 ult.mod. 4/2/22)
 	Utilizzo:	.\ListArchives.ps1
-	Info:		http://gioxx.org/tag/o365-powershell
+	Info:		https://gioxx.org/tag/o365-powershell
 #>
 
 ""; Write-Host "Archivi presenti su Exchange:"; "";
-
-<#
 Write-Host "Utenti" -f "Yellow"
-Get-Mailbox -ResultSize unlimited -Filter { ArchiveStatus -Eq "Active" -AND RecipientTypeDetails -eq 'UserMailbox'}
+Get-Mailbox -ResultSize unlimited | Where {$_.archivedatabase -ne $null -AND $_.RecipientTypeDetails -eq 'UserMailbox'} | Select Name,Alias,PrimarySmtpAddress,Database,ProhibitSendQuota,ExternalDirectoryObjectId | Out-GridView
 ""; Write-Host "Shared Mailbox" -f "Yellow"; ""
-Get-Mailbox -ResultSize unlimited -Filter { ArchiveStatus -Eq "Active" -AND RecipientTypeDetails -eq 'SharedMailbox'}
-#>
-
-Write-Host "Utenti" -f "Yellow"
-Get-Mailbox -ResultSize unlimited | Where {$_.archivedatabase -ne $null -AND $_.RecipientTypeDetails -eq 'UserMailbox'}
-""; Write-Host "Shared Mailbox" -f "Yellow"; ""
-Get-Mailbox -ResultSize unlimited | Where {$_.archivedatabase -ne $null -AND $_.RecipientTypeDetails -eq 'SharedMailbox'}
+Get-Mailbox -ResultSize unlimited | Where {$_.archivedatabase -ne $null -AND $_.RecipientTypeDetails -eq 'SharedMailbox'} | Select Name,Alias,PrimarySmtpAddress,Database,ProhibitSendQuota,ExternalDirectoryObjectId | Out-GridView
