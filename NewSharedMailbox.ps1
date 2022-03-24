5a7c51aa821b46f77f29e972c52f91fe810352a0
Gioxx <giovanni@solone.it>
2022-03-24 17:28:25 +0100

    "Fix" NSMbox + update PShell7

--- NewSharedMailbox.ps1
+++ NewSharedMailbox.ps1
@@ -4,11 +4,12 @@ OFFICE 365: Create New Shared Mailbox
 Autore originale: 		Alan Byrne
 Versione originale:		1.0
 Modifiche:						GSolone
-Versione:							0.9 rev1
+Versione:							0.10
 Utilizzo:							.\New-SharedMailbox.ps1
 Info:									https://gioxx.org/tag/o365-powershell
-Ultima modifica:			14-03-2022
+Ultima modifica:			24-03-2022
 Modifiche:
+		0.10- tolgo (commento) pausa dopo creazione Mailbox (ormai non ci sono più problemi dovuti a ritardi di risposta in PShell da M365, sperem).
 		0.9 rev1- correggo un ";" mancante.
 		0.9- imposto MessageCopyForSendOnBehalfEnabled e MessageCopyForSentAsEnabled entrambi a $True per salvare copia delle email inviate dalla casella di posta condivisa direttamente al suo interno e non solo nella Sent Items dell'utente che la sta utilizzando.
 		0.8- ho modificato l'esempio relativo all'indirizzo di posta da creare (ora info@contoso.com). Ho messo a posto parte dell'indentazione, e ho aggiunto un blocco che mostra i permessi applicati alla casella di posta quando si termina di aggiungerne.
@@ -33,7 +34,7 @@ Function Main {
 	try	{
 		""; Write-Host "Creo la Shared Mailbox, attendi." -f "Yellow"; ""
 		New-Mailbox -Name $SharedMailboxDisplayName -Alias $SharedMailboxAlias -Shared -PrimarySMTPAddress $SharedMailboxUserName
-		Start-Sleep -s 10
+		#Start-Sleep -s 10
 	} catch	{
 		Write-Host "Non riesco a creare la Shared Mailbox, riprovare." -f "Red"
 		write-host $error[0]
---
+++ tools/ps7update.ps1
@@ -0,0 +1,6 @@
+<#	O365 PShell Snippet:	PowerShell 7 Updater
+Autore (ver.-mod.):				GSolone (0.1 ult.mod. 21/3/22)
+Utilizzo:									.\ps7update.ps1
+Info:											https://gioxx.org/tag/o365-powershell
+#>
+iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
