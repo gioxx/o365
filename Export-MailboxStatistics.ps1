7a13a6bf4ef42b8f3529e76828beb5d4205e94cf
Gioxx <giovanni@solone.it>
2022-09-28 10:53:54 +0200

    MailboxStats + InPlace Archive

--- Export-MailboxStatistics.ps1
+++ Export-MailboxStatistics.ps1
@@ -3,16 +3,25 @@ OFFICE 365: Export Mailbox Statistics
 ----------------------------------------------------------------------------------------------------------------
 Autore originale:		Sconosciuto
 Autore:					    GSolone
-Versione:				    0.1
+Versione:				    0.2
 Utilizzo:				    .\Export-MailboxStatistics.ps1
 Info:					      https://gioxx.org/tag/o365-powershell
-Ultima modifica:		22-02-2022
+Ultima modifica:		28-09-2022
 Modifiche:
+  0.2- porto dentro qualsiasi tipo di mailbox.
 #>
 
 $DataOggi = Get-Date -format yyyyMMdd
 $CSV = "C:\temp\$($DataOggi)_MailboxSize.csv"
 
 ""; Write-Host "Esporto i dati, attendi" -f "Yellow"; "";
-Get-Mailbox -ResultSize Unlimited | where {$_.RecipientType -eq "UserMailbox"} | Select-Object DisplayName,servername,database,RecipientTypeDetails,PrimarySmtpAddress,@{Name='TotalItemSize(GB)'; expression={[math]::Round((((Get-MailboxStatistics $_.PrimarySmtpAddress).TotalItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},@{Name='ItemCount'; expression={(Get-MailboxStatistics $_.PrimarySmtpAddress).ItemCount}},IssueWarningQuota, ProhibitSendQuota | Export-Csv $CSV -Append -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+
+Get-Mailbox -ResultSize Unlimited |
+Select-Object DisplayName,
+servername,database,
+RecipientTypeDetails,PrimarySmtpAddress,
+@{Name='TotalItemSize(GB)'; expression={[math]::Round((((Get-MailboxStatistics $_.PrimarySmtpAddress).TotalItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},
+@{Name='ItemCount'; expression={(Get-MailboxStatistics $_.PrimarySmtpAddress).ItemCount}},
+IssueWarningQuota, ProhibitSendQuota |
+Export-Csv $CSV -Append -NoTypeInformation -Encoding UTF8 -Delimiter ";"
 Invoke-Item $CSV
--- tools/ListArchives.ps1
+++ tools/ListArchives.ps1
@@ -1,10 +1,19 @@
 <#	O365 PShell Snippet:	Get Online Archive List
-Autore (ver.-mod.):		    GSolone (0.3 ult.mod. 4/2/22)
+Autore (ver.-mod.):		    GSolone (0.4 ult.mod. 28/9/22)
 Utilizzo:				          .\ListArchives.ps1
 Info:					            https://gioxx.org/tag/o365-powershell
 #>
-""; Write-Host "Archivi presenti su Exchange:"; "";
-Write-Host "Utenti" -f "Yellow"
-Get-Mailbox -ResultSize unlimited | Where {$_.archivedatabase -ne $null -AND $_.RecipientTypeDetails -eq 'UserMailbox'} | Select Name,Alias,PrimarySmtpAddress,Database,ProhibitSendQuota,ExternalDirectoryObjectId | Out-GridView
-""; Write-Host "Shared Mailbox" -f "Yellow"; ""
-Get-Mailbox -ResultSize unlimited | Where {$_.archivedatabase -ne $null -AND $_.RecipientTypeDetails -eq 'SharedMailbox'} | Select Name,Alias,PrimarySmtpAddress,Database,ProhibitSendQuota,ExternalDirectoryObjectId | Out-GridView
+
+$DataOggi = Get-Date -format yyyyMMdd
+$CSV = "C:\temp\$($DataOggi)_ArchiveInPlace.csv"
+
+""; Write-Host "Esporto i dati, attendi" -f "Yellow"; "";
+Get-Mailbox -ResultSize Unlimited | Where {$_.ArchiveDatabase -ne $null} |
+Select-Object DisplayName,
+servername,database,
+RecipientTypeDetails,PrimarySmtpAddress,
+@{Name='TotalItemSize(GB)'; expression={[math]::Round((((Get-MailboxStatistics $_.PrimarySmtpAddress).TotalItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}},
+@{Name='ItemCount'; expression={(Get-MailboxStatistics $_.PrimarySmtpAddress).ItemCount}},
+IssueWarningQuota, ProhibitSendQuota |
+Export-Csv $CSV -Append -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+Invoke-Item $CSV
