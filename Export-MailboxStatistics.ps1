6e77c9892dab2386e5773abaaeb2f8d1bfa0d769
Gioxx <giovanni@solone.it>
2023-05-12 14:11:37 +0200

    Nuovo Export-MailboxStatistics.ps1

--- Export-MailboxStatistics.ps1
+++ Export-MailboxStatistics.ps1
@@ -1,21 +1,29 @@
 <#
-OFFICE 365: Export Mailbox Statistics
-----------------------------------------------------------------------------------------------------------------
-Autore originale:		Sconosciuto
-Autore:					    GSolone
-Versione:				    0.2
-Utilizzo:				    .\Export-MailboxStatistics.ps1
-Info:					      https://gioxx.org/tag/o365-powershell
-Ultima modifica:		28-09-2022
-Modifiche:
-  0.2- porto dentro qualsiasi tipo di mailbox.
-#>
+.SYNOPSIS
+    Script obtains mailbox statistics from an Exchange tenant on M365
+   
+.DESCRIPTION
+    Script obtains disk space occupancy statistics of mailboxes in an Exchange tenant (Microsoft 365) and collects them in a CSV file.
+    The exported data are the Display Name, Server Name, Database, Recipient Type (personal mailbox or shared mailbox), Primary SMTP Address, Issue Warning Quota and Prohibit Send Quota.
+    Script calculates disk space occupancy in GB.
 
-$DataOggi = Get-Date -format yyyyMMdd
-$CSV = "C:\temp\$($DataOggi)_MailboxSize.csv"
+.NOTES
+    Filename: Export-MailboxStatistics.ps1
+    Version: 0.3
+    Last modified: 12-05-2023
+    Author: GSolone
+    Blog: https://gioxx.org/tag/o365-powershell
+    Twitter: @gioxx    
 
-""; Write-Host "Esporto i dati, attendi" -f "Yellow"; "";
+.LINK
+    https://morgantechspace.com/2021/01/check-size-and-status-of-archive-mailbox-powershell.html
+#> 
 
+$Today = Get-Date -format yyyyMMdd
+$CSV = "C:\temp\$($Today)_MailboxSize.csv"
+
+<#
+# Old sourcecode, to be removed in the future
 Get-Mailbox -ResultSize Unlimited |
 Select-Object DisplayName,
 servername,database,
@@ -24,3 +32,51 @@ RecipientTypeDetails,PrimarySmtpAddress,
 IssueWarningQuota, ProhibitSendQuota |
 Export-Csv $CSV -Append -NoTypeInformation -Encoding UTF8 -Delimiter ";"
 Invoke-Item $CSV
+#>
+
+Set-Variable ProgressPreference Continue
+$Today = Get-Date -format yyyyMMdd
+$CSV = "C:\temp\$($Today)_MailboxSize.csv"
+$Result=@()
+$ProcessedCount = 0
+$Mailboxes = Get-Mailbox -ResultSize Unlimited
+$TotalMailboxes = $Mailboxes.Count
+
+$Mailboxes | Foreach-Object {
+    $ProcessedCount++
+    $Mbox = $_
+    $Size = $null
+    $ArchiveSize = $null
+    Write-Progress -Activity "Processing $Mbox" -Status "$ProcessedCount out of $TotalMailboxes completed" -PercentComplete (($ProcessedCount / $TotalMailboxes) * 100)
+
+    if ($Mbox.ArchiveStatus -eq "Active") {
+        $MailboxArchiveSize = Get-MailboxStatistics $Mbox.UserPrincipalName -Archive
+        
+        if ($MailboxArchiveSize.TotalItemSize -ne $null) {
+        $ArchiveSize = [math]::Round(($MailboxArchiveSize.TotalItemSize.ToString().Split('(')[1].Split(' ')[0].Replace(',','')/1GB),2)
+        } else {
+        $ArchiveSize = 0
+        }
+    }
+
+    $MailboxSize = [math]::Round((((Get-MailboxStatistics $Mbox.UserPrincipalName).TotalItemSize.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)
+
+    $Result += New-Object -TypeName PSObject -Property $([ordered]@{ 
+    UserName = $Mbox.DisplayName
+    ServerName = $Mbox.ServerName
+    Database = $Mbox.Database
+    RecipientTypeDetails = $Mbox.RecipientTypeDetails
+    PrimarySmtpAddress = $Mbox.PrimarySmtpAddress
+    MailboxSizeInGB = $MailboxSize
+    IssueWarningQuota = $Mbox.IssueWarningQuota
+    ProhibitSendQuota = $Mbox.ProhibitSendQuota
+    ArchiveStatus =$Mbox.ArchiveStatus
+    ArchiveName =$Mbox.ArchiveName
+    ArchiveState =$Mbox.ArchiveState
+    ArchiveMailboxSizeInGB = $ArchiveSize
+    ArchiveWarningQuota= if ( $Mbox.ArchiveStatus -eq "Active" ) { $Mbox.ArchiveWarningQuota } else { $null } 
+    ArchiveQuota = if ( $Mbox.ArchiveStatus -eq "Active" ) { $Mbox.ArchiveQuota } else { $null } 
+    AutoExpandingArchiveEnabled = $Mbox.AutoExpandingArchiveEnabled
+    })
+}
+$Result | Export-CSV $CSV -NoTypeInformation -Encoding UTF8 -Delimiter ";"
